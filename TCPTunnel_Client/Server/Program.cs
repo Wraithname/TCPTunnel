780d60450b7db9e117300c92962ed6cc9a9c6ef7
Wraithname <43811054+Wraithname@users.noreply.github.com>
2019-04-19 15:07:31 +0300

    Add files via upload

--- TCPTunnel_Client/Server/Program.cs
+++ TCPTunnel_Client/Server/Program.cs
@@ -11,6 +11,19 @@ namespace Server
 {
     class Program
     {
+        public static List<FileD> Files = new List<FileD>();
+        public struct FileD
+        {
+            public int ID;
+            public string FileName;
+            public string From;
+            public int FileSize;
+            public byte[] fileBuffer;
+        }
+        public static int CountUsers = 0;
+        public static List<User> UserList = new List<User>();
+        public static Socket ServerSocket;
+        public static bool Work = true;
         static void Main(string[] args)
         {
             Console.Write("Введите ip сервера: ");
@@ -18,19 +31,67 @@ namespace Server
             Console.Write("Введите порт сервера: ");
             int Port = Convert.ToInt32(Console.ReadLine());
             IPAddress address = IPAddress.Parse(Host);
-            Server.ServerSocket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            Server.ServerSocket.Bind(new IPEndPoint(address, Port));
-            Server.ServerSocket.Listen(100);
+            ServerSocket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            ServerSocket.Bind(new IPEndPoint(address, Port));
+            ServerSocket.Listen(100);
             Console.WriteLine($"Server has been started on {Host}:{Port}");
             Console.WriteLine("Waiting connections...");
-            while (Server.Work)
+            while (Work)
             {
-                Socket handle = Server.ServerSocket.Accept();
+                Socket handle = ServerSocket.Accept();
                 Console.WriteLine($"New connection: {handle.RemoteEndPoint.ToString()}");
                 new User(handle);
 
             }
             Console.WriteLine("Server closeing...");
+        }
+        public static void NewUser(User usr)
+        {
+            if (UserList.Contains(usr))
+                return;
+            UserList.Add(usr);
+        }
+        public static void EndUser(User usr)
+        {
+            if (!UserList.Contains(usr))
+                return;
+            UserList.Remove(usr);
+            usr.End();
+        }
+
+        public static User GetUser(string Name)
+        {
+            for (int i = 0; i < CountUsers; i++)
+            {
+                if (UserList[i].Username == Name)
+                    return UserList[i];
+            }
+            return null;
+        }
+        public static void SendUserList()
+        {
+            string userList = "#userlist|";
+
+            for (int i = 0; i < CountUsers; i++)
+            {
+                userList += UserList[i].Username + ",";
+            }
+
+            SendAllUsers(userList);
+        }
+        public static void SendAllUsers(byte[] data)
+        {
+            for (int i = 0; i < CountUsers; i++)
+            {
+                UserList[i].Send(data);
+            }
+        }
+        public static void SendAllUsers(string data)
+        {
+            for (int i = 0; i < CountUsers; i++)
+            {
+                UserList[i].Send(data);
+            }
         }
 
     }
